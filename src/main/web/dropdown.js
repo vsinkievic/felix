4c40dd25f968862880d2d816ff22b1638533f603
Linas <linasdaugela.online@gmail.com>
2018-07-22 21:12:37 +0300

    Autocomplete functionality.

--- src/main/web/dropdown.js
+++ src/main/web/dropdown.js
@@ -1,11 +1,16 @@
 $(document).ready(function() {
+	
+	$('#autocomplete').hide();
+	
     $('#inpName').keyup(function() {
-        let datalist = $('#json-datalist');
-        datalist.empty();
+    	$('#autocomplete').hide();
+    	$('#autocomplete').empty();
         let searchVal = $(this).val();
         let searchType = $('#inpType').val() + "";
         console.log(searchVal + " of type: " + searchType);
         if (searchVal.length > 2 && searchType != "0") {
+        	$('#autocomplete').show();
+        	let iwid = $('#inpName').width();
             $.ajax({
                 type: "POST",
                 url: "field_search.html",
@@ -22,12 +27,30 @@ $(document).ready(function() {
                             filteredResults.push(fResult);
                             };
                     };
-                    if (filteredResults.length > 1) {
+                    if (filteredResults.length > 0) {
+                    	let listHead = $('<lh>Available items:</lh>');
+                    	listHead.appendTo('#autocomplete');
                     	for (var i = 0; i < filteredResults.length; i++) {
-                    		let option = $('<option class="dropdown-option"></option>');
-                    		option.val(filteredResults[i]);
-                    		datalist.append(option);                    
+                    		let listItem = $('<li class="autocomplete-list-items"></li>');
+                    		listItem.attr('id', 'list' + i);
+                    		listItem.html(filteredResults[i]);
+                    		listItem.appendTo('#autocomplete');
+                            listItem.click(function() {
+                            	let cName = $(this).html();
+                            	$('#inpName').val(cName);
+                            	$("#xrefForm").submit();
+                            });
+                    	};
                     };
+                    let listHeight = -200;
+                    if (filteredResults.length < 20) {
+                    	listHeight = -1 * filteredResults.length * 10 - 5;
+                    };
+                	$('#autocomplete').css('transform', 'translate(100px, ' + listHeight + 'px)');
+                    if (filteredResults.length > 10) {
+                    	$('#autocomplete').css('overflow-y', 'scroll');
+                    } else {
+                    	$('#autocomplete').css('overflow-y', 'hidden');
                     };
                 },
                 error: function() {
